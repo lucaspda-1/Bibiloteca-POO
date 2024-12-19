e84d10fc3ae1eddc9cc208adc99a7b337406e0db
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2024-12-19 16:09:12 -0300

    MenuPrincipal.java
    criando a view do menu principal

--- src/biblioteca/view/MenuPrincipal.java
+++ src/biblioteca/view/MenuPrincipal.java
@@ -1,1 +1,16 @@
+package biblioteca.view;
 
+import biblioteca.bo.ItemBO;
+import biblioteca.bo.UsuarioBO;
+import biblioteca.exceptions.UsuarioNaoEncontradoException;
+import biblioteca.model.Item;
+import biblioteca.model.Usuario;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ItemBO itemBO = new ItemBO();
+        UsuarioBO usuarioBO = new UsuarioBO();
