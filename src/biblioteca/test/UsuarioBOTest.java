21f52512fd733b19a95fda4437de33d0045fddab
Klebertt <126410502+Klebertt@users.noreply.github.com>
2024-12-19 14:32:34 -0300

    Update UsuarioBOTest.java
    Criação da classe de testes UsuarioBOTest com inicialização do objeto usuarioBO

--- src/biblioteca/test/UsuarioBOTest.java
+++ src/biblioteca/test/UsuarioBOTest.java
@@ -1,1 +1,19 @@
+package biblioteca.test;
 
+import biblioteca.bo.UsuarioBO;
+import biblioteca.exceptions.UsuarioNaoEncontradoException;
+import biblioteca.model.Usuario;
+
+import java.sql.SQLException;
+
+public class UsuarioBOTest {
+    private UsuarioBO usuarioBO;
+
+    public UsuarioBOTest() {
+        usuarioBO = new UsuarioBO();
+    }
+
+    public void setUp() {
+        // Configuração inicial, se necessário
+    }
+}
