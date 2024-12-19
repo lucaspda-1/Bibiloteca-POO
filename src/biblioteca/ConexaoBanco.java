1dc60e33da3e76c3d7612eab24f183e0234877ec
lucaspda-1 <126190884+lucaspda-1@users.noreply.github.com>
2024-12-19 09:40:28 -0300

    Update ConexaoBanco.java

--- src/biblioteca/ConexaoBanco.java
+++ src/biblioteca/ConexaoBanco.java
@@ -1,1 +1,13 @@
+package biblioteca;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBanco {
+    private static final String URL = "jdbc:sqlite:biblioteca.db";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+}
