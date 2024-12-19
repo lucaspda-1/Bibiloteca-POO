baf5a83ff704331f7af38c653bd82b3c95047455
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2024-12-19 16:06:39 -0300

    UsuarioBO.java
    criando aos poucos o usuário galera

--- src/biblioteca/bo/UsuarioBO.java
+++ src/biblioteca/bo/UsuarioBO.java
@@ -1,1 +1,25 @@
+package biblioteca.bo;
+
+import biblioteca.dao.UsuarioDAO;
+import biblioteca.exceptions.UsuarioNaoEncontradoException;
+import biblioteca.model.Usuario;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UsuarioBO {
+    private UsuarioDAO usuarioDAO;
+
+    public UsuarioBO() {
+        this.usuarioDAO = new UsuarioDAO();
+    }
+
+    // Método para adicionar um usuário
+    public void adicionarUsuario(Usuario usuario) throws SQLException {
+        usuarioDAO.adicionarUsuario(usuario);
+    }
+
+    // Método para listar todos os usuários
+    public List<Usuario> listarUsuarios() throws SQLException {
+        return usuarioDAO.listarUsuarios();
+    }
 
