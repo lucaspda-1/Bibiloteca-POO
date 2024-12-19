8f3230f0ea32da754b97c095821cec060fe2407f
Paulo Junio <126300867+Paul1nhu@users.noreply.github.com>
2024-12-19 16:38:06 -0300

    Update UsuarioDAOTest.java

--- src/biblioteca/test/UsuarioDAOTest.java
+++ src/biblioteca/test/UsuarioDAOTest.java
@@ -4,3 +4,14 @@ import biblioteca.dao.UsuarioDAO;
 import biblioteca.model.Usuario;
 
 import java.sql.SQLException;
+
+public class UsuarioDAOTest {
+    private UsuarioDAO usuarioDAO;
+
+    public UsuarioDAOTest() {
+        usuarioDAO = new UsuarioDAO();
+    }
+
+    public void setUp() {
+        // Configuração inicial, se necessário
+    }
